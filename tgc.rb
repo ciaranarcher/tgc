92eab3eb507f7e11efaa1ef748efea4b3fedb1ff
Ciaran Archer <ciaran.archer@gmail.com>
2012-04-29 21:36:15 +0100

    Added layouts…ongoing…

--- tgc.rb
+++ tgc.rb
@@ -1,9 +1,14 @@
 # my_app.rb
 require 'sinatra/base'
-require 'sinatra/reloader'
+require 'sinatra/reloader' 
 
 class TGC < Sinatra::Base
   register Sinatra::Reloader if development?
+
+  before do
+    @path = request.path_info.gsub('/' , '')
+    @path = @path.empty? ? 'home' : @path
+  end
 
   get '/' do
     erb :home
