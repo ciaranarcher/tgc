768fa7b1698bd7fc55a9c3470286a34245f2f09f
Ciaran Archer <ciaran.archer@gmail.com>
2012-04-30 14:39:55 +0100

    Added display panels..

--- tgc.rb
+++ tgc.rb
@@ -61,6 +61,10 @@ class TGC < Sinatra::Base
     erb :posters
   end
 
+  get '/displaypanels' do
+    erb :displaypanels
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
