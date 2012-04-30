eb628ca108afc232aa21b74e7d145194fc337654
Ciaran Archer <ciaran.archer@gmail.com>
2012-04-30 14:26:07 +0100

    Added point of sale.

--- tgc.rb
+++ tgc.rb
@@ -44,6 +44,10 @@ class TGC < Sinatra::Base
     erb :contact
   end
 
+  get '/pointofsale' do
+    erb :pos
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
